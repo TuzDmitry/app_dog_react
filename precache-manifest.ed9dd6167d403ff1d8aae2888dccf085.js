72818a4bd35036fb14d4cb108b80b3299ecd8cce
TuzDmitry <dimatuz94@gmail.com>
2020-04-21 22:54:10 +0300

    Updates

---
+++ precache-manifest.ed9dd6167d403ff1d8aae2888dccf085.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63391c92e16a6206accc3979b156b1c1",
+    "url": "/app_dog_react/index.html"
+  },
+  {
+    "revision": "69c9fcc6f2f43700ec79",
+    "url": "/app_dog_react/static/css/main.fd4fc1e1.chunk.css"
+  },
+  {
+    "revision": "27d7c2750f1a85fed000",
+    "url": "/app_dog_react/static/js/2.eeb4a395.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app_dog_react/static/js/2.eeb4a395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c9fcc6f2f43700ec79",
+    "url": "/app_dog_react/static/js/main.e36deb39.chunk.js"
+  },
+  {
+    "revision": "f2d3d4d036dc2a975ecc",
+    "url": "/app_dog_react/static/js/runtime-main.df49bff8.js"
+  },
+  {
+    "revision": "75fa68d4d95a95385dabee5a76f50797",
+    "url": "/app_dog_react/static/media/dog.75fa68d4.mp3"
+  }
+]);
